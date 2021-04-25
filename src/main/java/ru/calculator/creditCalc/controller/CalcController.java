c49d1ee514015dc9937f9e5880db12207724e5ea
Evgen Krasnoshchekov <4445500@bk.ru>
2021-04-25 19:53:57 +0300

    Second commit

--- src/main/java/ru/calculator/creditCalc/controller/CalcController.java
+++ src/main/java/ru/calculator/creditCalc/controller/CalcController.java
@@ -14,37 +14,15 @@ public class CalcController{
 
     @GetMapping("/calc")
     public String NewCalculator(Calculator calculator) {
-      //  model.addAttribute("calculator", new Calculator());
         return "calc";
     }
 
     @PostMapping("/calc")
     public String AddCalc(@ModelAttribute("calculator") @Valid Calculator calculator, BindingResult bindingResult){
-
         if (bindingResult.hasErrors()) {
             return "calc";
         }
-
     return "calc";
     }
-
-   /* @RequestMapping(value="/calc", method=RequestMethod.GET)
-    public String showForm(Calculator calculator) {
-        return "form";
-    }*/
-
-   /* @RequestMapping(value="/calc", method= RequestMethod.POST)
-    public String checkPersonInfo( Calculator calculator, BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            return "form";
-        }
-        return "calc";
-    }*/
-
-/*    @GetMapping("/newCalc")
-    public String AddNewCalculator(Model model) {
-       model.addAttribute("calculator", new Calculator());
-        return "/newCalc";
-    }*/
 
 }
